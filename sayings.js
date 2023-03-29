2680f85b8819dc7369047d7e2c7ad5a75e67ee27
gbhatia478 <120049485+gbhatia478@users.noreply.github.com>
2023-03-29 14:39:54 -0700

    Add files via upload

---
+++ sayings.js
@@ -0,0 +1,17 @@
+ [
+     {
+         "id": 1,
+         "text": "The best way to predict the future is to invent it.",
+         "author": "Alan Kay"
+     },
+     {
+         "id": 2,
+         "text": "Don't watch the clock; do what it does. Keep going.",
+         "author": "Sam Levenson"
+     },
+     {
+         "id": 3,
+         "text": "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+         "author": "Winston Churchill"
+     }
+ ]
