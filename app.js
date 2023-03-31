62dd53f36e1177a4a92351cc17f09770d6c3e4b8
gbhatia478 <120049485+gbhatia478@users.noreply.github.com>
2023-03-30 23:53:06 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const apiKey = 'ba174bfbed46ac15dbcd7628397c28cc';
 const locationForm = document.getElementById('locationForm');
 const result = document.getElementById('result');
+const locationBtn = document.getElementById('locationBtn');
+
+locationBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const temp = ((data.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+        const conditions = data.weather[0].description;
+        result.innerHTML = `The temperature at latitude ${lat} and longitude ${lon} is ${temp} degrees Fahrenheit with ${conditions}.`;
+      })
+      .catch(error => console.log(error));
+  });
+});
 
 locationForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -11,9 +29,10 @@ locationForm.addEventListener('submit', function(event) {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      const temp = (data.main.temp - 273.15) * 1.8 + 32;
+      const temp = ((data.main.temp - 273.15) * 1.8 + 32).toFixed(1);
       const conditions = data.weather[0].description;
       result.innerHTML = `The temperature at latitude ${lat} and longitude ${lon} is ${temp} degrees Fahrenheit with ${conditions}.`;
     })
     .catch(error => console.log(error));
 });
+
